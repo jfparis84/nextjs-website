2cf440f512114e7a53bada144e447b0c42771a22
Jean-Francois Paris <jean-francois.paris@alterfina.ca>
2022-02-25 09:37:56 -0500

    initial commit.

---
+++ components/Layout/Default.jsx
@@ -0,0 +1,18 @@
+import NavBar from "../NavBar";
+
+export default function Layout({ title, children }) {
+  return (
+    <div className="min-h-full">
+      <NavBar />
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+      </main>
+
+      <footer className="bg-gray-800">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="text-white text-sm">©2022 par JF Paris. Créer avec <a href="https://nextjs.org/">Nextjs</a> / <a href="https://tailwindcss.com/">TailwindCSS</a></div>
+        </div>
+      </footer>
+    </div>
+  );
+}
