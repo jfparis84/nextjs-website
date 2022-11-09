d91a98b189d7f8d314ac5823db95af1c11a9c2cd
Jean-Francois Paris <jean-francois.paris@alterfina.ca>
2022-11-09 00:27:03 -0500

    Ajustement avec l'utilisation de stylesheet fontawesome.

--- components/Layout/Default.jsx
+++ components/Layout/Default.jsx
@@ -3,31 +3,30 @@ import NavBar from "../NavBar";
 
 export default function Layout({ title, children }) {
   return (
-      <>
+    <>
       <Head>
         <title>JF Paris</title>
-        <meta name="description" content="Avec plus de 15 ans d'expériences, je me passionne toujours pour les technologies de développement web." />
-        <link rel="icon" href="/favicon.ico" />
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
       </Head>
-    <div className="flex flex-col h-screen justify-between">
-      <NavBar />
+      <div className="flex flex-col h-screen justify-between">
+        <NavBar />
 
-      <main className="mb-auto">
-        <div className="max-w-7xl mx-4 sm:mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
-      </main>
-      
-      <footer className="bg-gray-800">
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="text-white text-sm">
-            ©2022 par JF Paris.<br />Créer avec{" "}
-            <a href="https://nextjs.org/">Nextjs</a> |{" "}
-            <a href="https://tailwindcss.com/">TailwindCSS</a>
+        <main className="mb-auto">
+          <div className="max-w-7xl mx-4 sm:mx-auto py-6 sm:px-6 lg:px-8">
+            {children}
           </div>
-        </div>
-      </footer>
-    </div>
+        </main>
+
+        <footer className="bg-gray-800">
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="text-white text-sm">
+              ©2022 par JF Paris.
+              <br />
+              Créer avec <a href="https://nextjs.org/">Nextjs</a> |{" "}
+              <a href="https://tailwindcss.com/">TailwindCSS</a>
+            </div>
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="h-full">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Avec plus de 15 ans d'expériences, je me passionne toujours pour les technologies de développement web."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        />
+      </Head>
       <body className="h-full">
         <Main />
         <NextScript />
