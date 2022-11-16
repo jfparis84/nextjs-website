1c2e72d9b2abbc36a54a691789c9aab6d83dcac8
Jean-Francois Paris <jean-francois.paris@alterfina.ca>
2022-11-15 21:40:57 -0500

    Install and configure GTM.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
-import '../styles/globals.scss'
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+import '../styles/globals.scss';
+
 
 function MyApp({ Component, pageProps }) {
+
+  // Google Tag Manager
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID });
+  }, []);
+
   return <Component {...pageProps} />
 }
 
