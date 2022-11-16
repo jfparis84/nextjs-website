fa1b504e8f456280f2c1318cc3d6499d491835cd
Jean-Francois Paris <jean-francois.paris@alterfina.ca>
2022-11-15 22:33:37 -0500

    Ajustement sur le footer.

--- components/Layout/Default.jsx
+++ components/Layout/Default.jsx
@@ -17,7 +17,7 @@ export default function Layout({ title, children }) {
         </main>
 
         <footer className="bg-gray-800">
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <div className="text-white text-sm">
               ©2022 par JF Paris.
               <br />
