f9994e30e7c7ce80d718ad224009833a7aebe4a0
Jean-Francois Paris <jfparis84@gmail.com>
2025-02-23 23:52:20 -0500

    Add some animation on the menu. (#6)
    Co-authored-by: jfparis <jean-francois.paris@groupealesco.ca>

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { AnimatePresence, easeOut, motion } from 'framer-motion';
+import { Fragment } from 'react';
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 import Link from "next/link";
@@ -43,36 +45,66 @@ export default function NavBar() {
                 {/* Mobile menu button */}
                 <DisclosureButton className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                   <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
+
+                  <div>
+                    <motion.div
+                      animate={{
+                        rotate: open ? 45 : 0,
+                        y: open ? 6 : 0,
+                      }}
+                      className="w-6 h-0.5 bg-white mb-1"
+                    />
+                    <motion.div
+                      animate={{
+                        opacity: open ? 0 : 1,
+                      }}
+                      className="w-6 h-0.5 bg-white mb-1"
+                    />
+                    <motion.div
+                      animate={{
+                        rotate: open ? -45 : 0,
+                        y: open ? -6 : 0,
+                      }}
+                      className="w-6 h-0.5 bg-white"
+                    />
+                  </div>
                 </DisclosureButton>
               </div>
             </div>
           </div>
 
-          <DisclosurePanel className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 border-t-2 border-red-500">
-              {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as={Link}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
+          <AnimatePresence>
+            {open && (
+              <DisclosurePanel static as={Fragment} className="md:hidden">
+                <motion.div
+                  initial={{ opacity: 0, y: -24 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -24 }}
+                  transition={{ duration: 0.2, ease: easeOut }}
+                  className="origin-top"
                 >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-          </DisclosurePanel>
+                  <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 border-t-2 border-red-500">
+                    {navigation.map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as={Link}
+                        href={item.href}
+                        className={classNames(
+                          item.regexp.test(router.asPath)
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "block px-3 py-2 rounded-md text-base font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </div>
+                </motion.div>
+              </DisclosurePanel>
+            )}
+          </AnimatePresence>
         </>
       )}
     </Disclosure>
